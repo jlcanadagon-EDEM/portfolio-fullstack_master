8da09400f7abb666ddc1c8168eb8cc47155aea1c
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 18:56:06 +0200

    Resolver ejercicio 16 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-16.js
@@ -0,0 +1,19 @@
+/*
+Ejercicio 16
+
+Usa un bucle for...of para recorrer todos los destinos
+del array y muestra cada valor por consola.
+*/
+
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (const place of placesToTravel) {
+  console.log(place);
+}
