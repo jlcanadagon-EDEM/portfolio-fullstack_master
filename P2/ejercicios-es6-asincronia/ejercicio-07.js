7276280717af7def72f9c2c6afffec71b2468754
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 19:40:19 +0200

    Resolver ejercicio ES6 7

---
+++ P2/ejercicios-es6-asincronia/ejercicio-07.js
@@ -0,0 +1,52 @@
+/*
+Ejercicio ES6 7
+
+Filtra los videojuegos que pertenezcan al género RPG
+y calcula la media de sus puntuaciones usando reduce().
+*/
+
+const videogames = [
+  {
+    name: "Final Fantasy VII",
+    genders: ["RPG"],
+    score: 9.5,
+  },
+  {
+    name: "Assasins Creed Valhalla",
+    genders: ["Aventura", "RPG"],
+    score: 4.5,
+  },
+  {
+    name: "The Last of Us 2",
+    genders: ["Acción", "Aventura"],
+    score: 9.8,
+  },
+  {
+    name: "Super Mario Bros",
+    genders: ["Plataforma"],
+    score: 8.5,
+  },
+  {
+    name: "Genshin Impact",
+    genders: ["RPG", "Aventura"],
+    score: 7.5,
+  },
+  {
+    name: "Legend of Zelda: Breath of the wild",
+    genders: ["RPG"],
+    score: 10,
+  },
+];
+
+const rpgVideogames = videogames.filter((videogame) =>
+  videogame.genders.includes("RPG")
+);
+
+const totalRpgScore = rpgVideogames.reduce((total, videogame) => {
+  return total + videogame.score;
+}, 0);
+
+const averageRpgScore = totalRpgScore / rpgVideogames.length;
+
+console.log(rpgVideogames);
+console.log(`Media de los videojuegos RPG: ${averageRpgScore}`);
