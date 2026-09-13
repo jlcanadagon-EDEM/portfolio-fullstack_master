772d4fc0e551998df945ac62edb7cbb2344d18fe
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 19:02:19 +0200

    Resolver ejercicio 24 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-24.js
@@ -0,0 +1,28 @@
+/*
+Ejercicio 24
+
+Recorre los personajes de Star Wars, selecciona aquellos
+cuya especie sea "Human" y guárdalos en humanCharacters.
+*/
+
+const characters = [
+  { name: "Luke Skywalker", species: "Human" },
+  { name: "Darth Vader", species: "Human" },
+  { name: "Chewbacca", species: "Wookiee" },
+  { name: "Leia Organa", species: "Human" },
+  { name: "R2-D2", species: "Droid" },
+  { name: "C-3PO", species: "Droid" },
+  { name: "Obi-Wan Kenobi", species: "Human" },
+  { name: "Yoda", species: "Unknown" },
+  { name: "Han Solo", species: "Human" },
+];
+
+const humanCharacters = [];
+
+for (const character of characters) {
+  if (character.species === "Human") {
+    humanCharacters.push(character);
+  }
+}
+
+console.log(humanCharacters);
