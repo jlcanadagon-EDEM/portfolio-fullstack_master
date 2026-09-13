579cbb1e2bf664a7c7f69db600e8d930ffa9b208
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 18:42:17 +0200

    Resolver ejercicio 3 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-03.js
@@ -0,0 +1,35 @@
+/*
+Ejercicio 3
+
+3.1 Multiplica 10 por 5 y muestra el resultado por consola.
+
+3.2 Divide 10 entre 2 y muestra el resultado por consola.
+
+3.3 Muestra el resto de dividir 15 entre 9.
+
+3.4 Usa el operador de asignación correcto para obtener o = 15,
+partiendo de p = 10 y j = 5.
+
+3.5 Usa el operador de asignación correcto para obtener i = 50,
+partiendo de c = 10 y m = 5.
+*/
+
+console.log(10 * 5);
+console.log(10 / 2);
+console.log(15 % 9);
+
+let p = 10;
+const j = 5;
+let o = p;
+
+o += j;
+
+console.log(o);
+
+let c = 10;
+const m = 5;
+let i = c;
+
+i *= m;
+
+console.log(i);
