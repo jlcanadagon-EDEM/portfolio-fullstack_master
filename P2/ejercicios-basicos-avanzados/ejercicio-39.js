254f991b651812e2b63b5125903ab7d184ece43a
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 19:12:52 +0200

    Resolver ejercicio 39 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-39.js
@@ -0,0 +1,55 @@
+/*
+Ejercicio 39
+
+Utiliza for...of y for...in para contar cuántas veces
+ha sido añadido cada sonido a favoritos.
+*/
+
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const soundCounter = {};
+
+for (const user of users) {
+  for (const soundName in user.favoritesSounds) {
+    if (soundCounter[soundName]) {
+      soundCounter[soundName]++;
+    } else {
+      soundCounter[soundName] = 1;
+    }
+  }
+}
+
+console.log(soundCounter);
