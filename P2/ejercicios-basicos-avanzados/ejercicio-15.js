c3b26064c7602f11c6336757a9394c3c9c137021
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 18:55:36 +0200

    Resolver ejercicio 15 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-15.js
@@ -0,0 +1,23 @@
+/*
+Ejercicio 15
+
+Recorre el array y muestra por consola todos los valores
+que incluyan la palabra "Camiseta".
+
+Utiliza el método includes().
+*/
+
+const products = [
+  "Camiseta de Metallica",
+  "Pantalón vaquero",
+  "Gorra de beisbol",
+  "Camiseta de Basket",
+  "Cinturón de Orión",
+  "AC/DC Camiseta",
+];
+
+for (const product of products) {
+  if (product.includes("Camiseta")) {
+    console.log(product);
+  }
+}
