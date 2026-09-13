d87ac7858eebfd95e397e930ce6ca36c3b4b265c
jlcanadagon-EDEM <jlcanadagon@gmail.com>
2026-09-13 18:35:30 +0200

    Resolver ejercicio 1 de JavaScript

---
+++ P2/ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,29 @@
+/*
+Ejercicio 1
+
+1. Crea una variable llamada miSuperheroeFavorito
+   y asigna el valor "Hulk".
+
+2. Crea una variable llamada numeroFavorito
+   y asigna el valor 50.
+
+3. Crea una variable llamada altura y asigna el valor 5.
+   Crea una variable llamada longitud y asigna el valor 10.
+
+4. Crea una variable llamada suma y asigna
+   el valor de altura + longitud.
+
+5. Muestra todas las variables por consola.
+*/
+
+const miSuperheroeFavorito = "Hulk";
+const numeroFavorito = 50;
+const altura = 5;
+const longitud = 10;
+const suma = altura + longitud;
+
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
